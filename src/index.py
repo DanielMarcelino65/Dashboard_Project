125efd44d105c3258fafaf23ad95b59f7202f023
DanielMarcelino65 <danmarcelino6573@gmail.com>
2023-08-29 16:44:20 -0300

    :wrench: chore update index.py with importation of the new module

--- src/index.py
+++ src/index.py
@@ -1,5 +1,9 @@
 from app import * 
 from data import paid_games_and_score, free_games_and_score, sorted_games_and_score, null_games_and_score
+from dash import html, dcc, Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+from dash_bootstrap_templates import ThemeSwitchAIO
 
 if __name__ == '__main__':
     app.run_server(debug=True, port = 8050)
