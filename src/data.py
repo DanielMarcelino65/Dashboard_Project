ab62024f5ddc9ee8cc4d06ce8aa2f39aa1ec65c1
DanielMarcelino65 <danmarcelino6573@gmail.com>
2023-08-30 12:56:40 -0300

    :cronstuction: Update of dashboard-test with intereactive graph

--- src/data.py
+++ src/data.py
@@ -13,21 +13,23 @@ df['score'] = df['metacritic'].apply(lambda x: x['score'])
 df_sorted = df.sort_values(by='score', ascending=False)
 df_free = df_sorted[df_sorted['is_free'] == True]
 df_paid = df_sorted[df_sorted['is_free'] == False]
-df_samePublisher = df_sorted[df_sorted['publishers'].apply(lambda x: isinstance(x, list) and 'Valve' in x)]
 
 paid_games_and_score = []
 free_games_and_score = []
 sorted_games_and_score = []
 null_games_and_score = []
-
+counter = 0
         
 for index, row in df.iterrows():
     name = row['name']
     score = row['metacritic']['score']
     if score == 0:
+        counter = counter + 1
         game_info = f"Jogo: {name}, Nota: {score}"
         null_games_and_score.append(game_info)
         # print(game_info)
+        # print(counter)
+    
 
 for index, row in df_sorted.iterrows():
     name = row['name']
@@ -52,3 +54,25 @@ for index, row in df_paid.iterrows():
         game_info = f"Jogo: {name}, Nota: {score}"
         paid_games_and_score.append(game_info)
         # print(game_info)
+
+
+paid_games = []
+paid_scores = []
+for game_info in paid_games_and_score:
+    parts = game_info.split(', ')
+    if len(parts) >= 2:
+        game_name = parts[0].split(': ')[1]
+        game_score_parts = parts[1].split(': ')
+        if len(game_score_parts) >= 2:
+            game_score = game_score_parts[1]
+            paid_games.append(game_name)
+            paid_scores.append(game_score)
+            # print("Game Name:", game_name)
+            # print("Game Score:", game_score)
+
+df_paid_games_score = pd.DataFrame({'Jogos': paid_games, 'Notas': paid_scores})
+df_paid_games_score = df_paid_games_score.drop_duplicates(subset=['Jogos'], keep='first')
+note_counts = df_paid_games_score['Notas'].value_counts().reset_index()
+note_counts.columns = ['Notas', 'Count']
+df_paid_games_score = df_paid_games_score.merge(note_counts, on='Notas', how='right')
+df_paid_games_score = df_paid_games_score.sort_values(by='Notas', ascending=False)
--- src/index.py
+++ src/index.py
@@ -1,9 +1,81 @@
 from app import * 
-from data import paid_games_and_score, free_games_and_score, sorted_games_and_score, null_games_and_score
+from data import df_paid_games_score, note_counts
 from dash import html, dcc, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 from dash_bootstrap_templates import ThemeSwitchAIO
+import pandas as pd
+
+url_theme1 = dbc.themes.VAPOR
+url_theme2 = dbc.themes.FLATLY
+
+template_theme1 = 'vapor'
+template_theme2 = 'flatly'
+
+game_options = [{'label': x, 'value': x} for x in df_paid_games_score['Jogos'].unique()]
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            ThemeSwitchAIO(aio_id='theme', themes=[url_theme1, url_theme2]),
+        ]),
+        dbc.Col([
+            html.H1('Título do Gráfico'),
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='bar_graph')
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(id='game_picker', options=game_options, multi=True, value=[game['label'] for game in game_options[:10] + game_options[-10:]]),
+        ])
+    ]),
+    dbc.Row([
+        dcc.Graph(id='scatter_graph')
+    ]),
+    dbc.Row([
+        dcc.Graph(id='line_graph'),  # Novo componente para o gráfico de linhas
+    ]),
+])
+
+@app.callback(
+    Output('bar_graph', 'figure'),
+    Input(ThemeSwitchAIO.ids.switch('theme'), 'value'),
+    Input('game_picker', 'value')
+)
+def update_bar_graph(toggle, games):
+    templates = template_theme1 if toggle else template_theme2
+    filtered_df = df_paid_games_score[df_paid_games_score['Jogos'].isin(games)]
+    
+    fig = px.bar(filtered_df, x='Jogos', y='Notas', title='Notas dos Jogos', color='Notas')
+    return fig
+
+@app.callback(
+    Output('scatter_graph', 'figure'),  # Update the correct graph id here
+    Input(ThemeSwitchAIO.ids.switch('theme'), 'value')
+)
+def update_scatter_graph(toggle):
+    templates = template_theme1 if toggle else template_theme2
+    fig = px.scatter(df_paid_games_score, x='Notas', y='Jogos', color='Notas', title='Outro Título')
+    fig.update_layout(xaxis_title='Jogos', yaxis_title='Notas', xaxis_tickangle=-45)
+
+    return fig
+
+@app.callback(
+    Output('line_graph', 'figure'),  # Update the correct graph id here
+    Input(ThemeSwitchAIO.ids.switch('theme'), 'value')
+)
+def update_line_graph(toggle):
+    templates = template_theme1 if toggle else template_theme2
+
+    fig = px.line(df_paid_games_score, x='Notas', y='Jogos', color='Notas', title='Outro Título')
+    fig.update_layout(xaxis_title='Jogos', yaxis_title='Notas', xaxis_tickangle=-45)
+
+    return fig
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port = 8050)
+    app.run_server(debug=True, port = 8051)
