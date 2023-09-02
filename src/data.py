6837191ca303f2140b913571aca4f277f804b852
DanielMarcelino65 <danmarcelino6573@gmail.com>
2023-09-01 22:38:32 -0300

    :construction: Update index and data files :construction:

--- src/data.py
+++ src/data.py
@@ -1,3 +1,4 @@
+
 import json
 import pandas as pd
 import plotly.express as px
@@ -5,26 +6,26 @@ import plotly.express as px
 def json_loads_single_quotes(text):
     return json.loads(text.replace("'", "\""))
 
+## Carregando o dataset e separando dados necessários
 df = pd.read_csv('./assets/steam_app_data.csv')
-
-years = df['release_date'].apply(lambda x: str(x).split(',')[-1].replace("'", "").replace("}", "").strip())
 games = df['name']
-df_years_games = pd.DataFrame({'Anos': years, 'Jogos': games})
-count_by_year = df_years_games['Anos'].value_counts().sort_index()
 df['metacritic'] = df['metacritic'].fillna('{"score": 0}')
 df['metacritic'] = df['metacritic'].apply(json_loads_single_quotes)
 df['score'] = df['metacritic'].apply(lambda x: x['score'])
 
+## Separando os jogos pagos e gratuitos
 df_sorted = df.sort_values(by='score', ascending=False)
 df_free = df_sorted[df_sorted['is_free'] == True]
 df_paid = df_sorted[df_sorted['is_free'] == False]
 
+## Separando os jogos pagos e gratuitos e suas notas
 paid_games_and_score = []
 free_games_and_score = []
 sorted_games_and_score = []
 null_games_and_score = []
 counter = 0
-        
+
+##Laços de repetição para salvar os dados necessários do dataset
 for index, row in df.iterrows():
     name = row['name']
     score = row['metacritic']['score']
@@ -34,7 +35,6 @@ for index, row in df.iterrows():
         null_games_and_score.append(game_info)
         # print(game_info)
         # print(counter)
-    
 
 for index, row in df_sorted.iterrows():
     name = row['name']
@@ -61,6 +61,7 @@ for index, row in df_paid.iterrows():
         # print(game_info)
 
 
+## Separando os jogos pagos e suas notas
 paid_games = []
 paid_scores = []
 for game_info in paid_games_and_score:
@@ -75,6 +76,8 @@ for game_info in paid_games_and_score:
             paid_games.append(game_name)
             paid_scores.append(game_score)
 
+
+## Separando os jogos gratuitos e suas notas 
 free_games = []
 free_scores = []
 for game_info in free_games_and_score:
@@ -88,16 +91,35 @@ for game_info in free_games_and_score:
             game_score = game_info[game_score_start + len('Nota: '):].strip()
             free_games.append(game_name)
             free_scores.append(game_score)
+
+## Tratamento dos Jogos Gratuitos
 df_free_games_score = pd.DataFrame({'Jogos': free_games, 'Notas': free_scores})
+
+## Definindo a contagem das notas dos jogos gratuitos
 free_note_counts = df_free_games_score['Notas'].value_counts().reset_index()
 free_note_counts.columns = ['Notas', 'Count']
 non_zero_free_note_counts = free_note_counts[free_note_counts['Count'] > 0]
 non_zero_free_note_counts = non_zero_free_note_counts.sort_values(by='Notas', ascending=False)
+
+## Tratamento dos Jogos Pagos
 df_paid_games_score = pd.DataFrame({'Jogos': paid_games, 'Notas': paid_scores})
 df_paid_games_score = df_paid_games_score.drop_duplicates(subset=['Jogos'], keep='first')
+
+## Definindo a contagem das notas dos jogos pagos
 note_counts = df_paid_games_score['Notas'].value_counts().reset_index()
 note_counts.columns = ['Notas', 'Count']
+
+## Definindo a média das notas dos jogos pagos e gratuitos e acima de 0
 non_zero_note_counts = note_counts[note_counts['Count'] > 0]
 non_zero_note_counts = non_zero_note_counts.sort_values(by='Notas', ascending=False)
 df_paid_games_score = df_paid_games_score.merge(note_counts, on='Notas', how='right')
 df_paid_games_score = df_paid_games_score.sort_values(by='Notas', ascending=False)
+
+## Definindo a média das notas dos jogos pagos e gratuitos
+media_jogos_pagos_noround = df_paid_games_score['Notas'].astype(float).mean()
+media_jogos_gratuitos_noround = df_free_games_score['Notas'].astype(float).mean()
+media_jogos_pagos = round(media_jogos_pagos_noround, 2)
+media_jogos_gratuitos = round(media_jogos_gratuitos_noround, 2)
+# print(media_jogos_pagos)
+# print(media_jogos_gratuitos)
+ 
--- src/index.py
+++ src/index.py
@@ -1,29 +1,35 @@
+##Importação das bibliotecas
 from app import * 
-from data import df_paid_games_score, non_zero_note_counts, non_zero_free_note_counts, df_free_games_score
+from data import df_paid_games_score, non_zero_note_counts, non_zero_free_note_counts, df_free_games_score, media_jogos_gratuitos, media_jogos_pagos
 from dash import html, dcc, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 from dash_bootstrap_templates import ThemeSwitchAIO
 import pandas as pd
 
+##Definição do tema
 url_theme1 = dbc.themes.VAPOR
 url_theme2 = dbc.themes.FLATLY
 
+##Definição do layout
 content = ''
 content2 = ''
 template_theme1 = 'vapor'
 template_theme2 = 'flatly'
 
+##Função para truncar o texto do eixo X do grafico de barras
 def truncate_text(text, max_length=30):
     if len(text) > max_length:
         return text[:max_length] + "..."
     return text
 
+##Definição das opções dos dropdowns
 game_options = [{'label': x, 'value': x} for x in df_paid_games_score['Jogos'].unique()]
 free_game_options = [{'label': x, 'value': x} for x in df_free_games_score['Jogos'].unique()]
 note_options =[{ 'label': x, 'value': x} for x in non_zero_note_counts['Notas'].unique()]
 free_note_options =[{ 'label': x, 'value': x} for x in non_zero_free_note_counts['Notas'].unique()]
 
+##Definição do layout
 app.layout = dbc.Container([
     dbc.Row([
             ThemeSwitchAIO(aio_id='theme', themes=[url_theme1, url_theme2]),
@@ -48,6 +54,7 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dcc.Graph(id='pie_chart'),
+            html.P('Escolha as notas para o gráfico de pizza', className='text-center', style={'position': 'relative', 'bottom': '30px', 'height': '0px'}),
             dcc.Dropdown(id='game_picker_pie', options=note_options, multi=True, value=[note['label'] for note in note_options[:5] + note_options[-5:]]),
         ], width=6),
         dbc.Col([
@@ -58,6 +65,7 @@ app.layout = dbc.Container([
     ]),
 ])
 
+##Callback e função do botao
 @app.callback(
     Output('game_picker', 'style'),
     Output('show_hide', 'children'),
@@ -75,6 +83,7 @@ def show_hide(n_clicks, children):
     
     return game_picker_style, content
 
+##Callback e função do botao2
 @app.callback(
     Output('game_picker2', 'style'),
     Output('show_hide2', 'children'),
@@ -92,6 +101,7 @@ def show_hide2(n_clicks, children):
     
     return game_picker_style, content2
 
+##Callback e função do grafico de barras
 @app.callback(
     Output('bar_graph', 'figure'),
     Input(ThemeSwitchAIO.ids.switch('theme'), 'value'),
@@ -108,8 +118,10 @@ def update_bar_graph(toggle, games):
     filtered_df.loc[:, 'Notas'] = filtered_df['Notas'].astype(float)
     
     fig = px.bar(filtered_df, x='Jogos', y='Notas', title='Metacritic de Jogos Pagos', color='Jogos', template=templates)
+    fig.add_trace(go.Scatter(x=filtered_df['Jogos'], y=[media_jogos_pagos] * len(filtered_df['Jogos']), name='Média', hoverinfo='text', hovertext=f'Media: {media_jogos_pagos}', mode='lines', line=dict(color='black', width=2, dash='dash')))
     return fig
 
+##Callback e função do grafico de barras2
 @app.callback(
     Output('bar_graph2', 'figure'),
     Input(ThemeSwitchAIO.ids.switch('theme'), 'value'),
@@ -125,8 +137,10 @@ def update_bar_graph(toggle, games):
     filtered_df.loc[:, 'Notas'] = filtered_df['Notas'].astype(float)
     
     fig = px.bar(filtered_df, x='Jogos', y='Notas', title='Metacritic de Jogos Gratuitos', color='Jogos', template=templates)
+    fig.add_trace(go.Scatter(x=filtered_df['Jogos'], y=[media_jogos_gratuitos] * len(filtered_df['Jogos']), name='Média', hoverinfo='text', hovertext=f'Media: {media_jogos_gratuitos}', mode='lines', line=dict(color='black', width=2, dash='dash')))
     return fig
 
+##Callback e função do grafico de pizza
 @app.callback(
     Output('pie_chart', 'figure'),
     Input(ThemeSwitchAIO.ids.switch('theme'), 'value'),
@@ -137,11 +151,12 @@ def update_pie_chart(toggle, selected_games):
 
     if selected_games:
         filtered_df = non_zero_note_counts[non_zero_note_counts['Notas'].isin(selected_games)]
-        fig = px.pie(filtered_df, names='Notas', values='Count', title='Pizza de Jogos Pagos',template=templates)
+        fig = px.pie(filtered_df, names='Notas', values='Count', title='Pizza de Notas Jogos Pagos',template=templates)
 
 
         return fig
-    
+
+##Callback e função do grafico de pizza2
 @app.callback(
     Output('pie_chart2', 'figure'),
     Input(ThemeSwitchAIO.ids.switch('theme'), 'value'),
